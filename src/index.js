c5c948b9589c38335ec8ce7d1d30ac6b8563d12a
ozguryesilbas@live.com <ozgur1586>
2020-03-31 16:49:40 +0300

    uygulamanın çalıştığı kök javascript dosyası değiştirildi

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Text, View} from "react-native";
+
+class App extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Hello from React Native</Text>
+            </View>
+        );
+    }
+};
+
+export default App;
+
